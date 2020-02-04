b3f3673681a9acbe12a13d7fc3c74fcbbdbe40df
Ana Carolina <cscarol20@gmail.com>
2020-02-04 18:40:06 -0300

    fim do app 2 - frases do dia

--- app2/index.js
+++ app2/index.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import {AppRegistry, View, Text, Image} from 'react-native';
+import {
+  AppRegistry,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 
+const gerarNovaFrase = () => {
+  var numeroAleatorio = Math.random();
+  numeroAleatorio = Math.floor(numeroAleatorio * 5);
+
+  var frases = Array();
+  frases[0] =
+    'As mais lindas palavras de amor são ditas no silêncio de um olhar.';
+  frases[1] = 'É um amor pobre aquele que se pode medir.';
+  frases[2] =
+    'Choramos ao nascer porque chegamos a este imenso cenário de dementes.';
+  frases[3] = 'O diabo pode citar as Escrituras quando isso lhe convém.';
+  frases[4] = 'A alegria evita mil males e prolonga a vida.';
+
+  var fraseEscolhida = frases[numeroAleatorio];
+
+  Alert.alert(fraseEscolhida);
+};
 // Cria componente
 const App = () => {
-  const {principal} = Estilos;
+  const {principal, botao, textoBotao} = Estilos;
   return (
     <View style={principal}>
-      {/* <Image source={require('./img/uvas.png')} /> */}
-      <Image source={require({})} />
+      <Image source={require('./img/logo.png')} />
+      <TouchableOpacity onPress={gerarNovaFrase} style={botao}>
+        <Text style={textoBotao}>Nova Frase</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -16,34 +42,20 @@ AppRegistry.registerComponent('app2', () => App);
 // Formatação
 const Estilos = {
   principal: {
-    paddingTop: 40,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   botao: {
-    backgroundColor: '#48BBEC',
-    padding: 10,
-    borderColor: '#1d8eb8',
-    borderWidth: 1,
+    backgroundColor: '#538530',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    marginTop: 20,
     borderRadius: 10,
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.4,
   },
   textoBotao: {
     color: '#fff',
     fontSize: 16,
-    alignSelf: 'center',
     fontWeight: 'bold',
-  },
-  topo: {
-    flex: 1,
-    backgroundColor: '#3388ff',
-  },
-  conteudo: {
-    flex: 12,
-    backgroundColor: '#f0f',
-  },
-  rodape: {
-    flex: 1,
-    backgroundColor: '#cacaca',
   },
 };
